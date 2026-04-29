e6193e9c7b4c81c14565206f4e462a33e06db26a
melissa <melissagh3009@gmail.com>
2026-04-29 14:16:50 -0400

    Update model/post.go - added support for personalized background and text color - defaults are pastel yellow and dark gray respectively

--- internal/model/post.go
+++ internal/model/post.go
@@ -10,6 +10,8 @@ type Post struct {
 	ID        string         `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"  json:"id"`
 	Content   string         `gorm:"not null"                                        json:"content"`
 	Drawing   *string        `gorm:"type:text"                                       json:"drawing,omitempty"`
+	BgColor   string         `gorm:"type:char(7);default:'#f7f19e'"					 json:"bg"` // pastel yellow
+	FgColor   string         `gorm:"type:char(7);default:'#1e1f24'"					 json:"fg"` // dark gray
 	Likes     int            `gorm:"default:0"                                       json:"likes"`
 	Status    string         `gorm:"default:pending"                                 json:"status,omitempty"`
 	CreatedAt time.Time      `                                                       json:"created_at"`
