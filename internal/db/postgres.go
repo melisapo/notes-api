16abdb4178832a2e550450e12d6149739dd5db2b
melissa <melissagh3009@gmail.com>
2026-04-28 21:28:04 -0400

    Create postgres.go

---
+++ internal/db/postgres.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+
+	"mota/internal/model"
+)
+
+func Connect() (*gorm.DB, error) {
+	dsn := os.Getenv("DATABASE_URL")
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(
+		&model.Post{},
+		&model.PostLike{},
+		&model.PostReport{},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
